504bd07b4db9f73ee0c989b886bf6c2dcc68b95b
Connie Hirsch <connie.hirsch@gmail.com>
2019-03-23 17:31:40 -0400

    Proper shelter setup

--- src/app/shared/shelter.model.ts
+++ src/app/shared/shelter.model.ts
@@ -1,27 +1,11 @@
 export class Shelter {
-  public name: string;
-  public street: string;
-  public city: string;
-  public state: string;
-  public zip: string;
-  public phone: string;
-  public email: string;
-
   constructor(
-    name: string,
-    street: string,
-    city: string,
-    state: string,
-    zip: string,
-    phone: string,
-    email: string
-  ) {
-    this.name = name;
-    this.street = street;
-    this.city = city;
-    this.state = state;
-    this.zip = zip;
-    this.phone = phone;
-    this.email = email;
-  }
+    public name: string,
+    public street: string,
+    public city: string,
+    public state: string,
+    public zip: string,
+    public phone: string,
+    public email: string
+  ) {}
 }
--- src/app/shelter-list/shelter-list.component.ts
+++ src/app/shelter-list/shelter-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Shelter } from "../shared/shelter.model";
 
 @Component({
   selector: "app-shelter-list",
@@ -6,7 +7,35 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./shelter-list.component.css"]
 })
 export class ShelterListComponent implements OnInit {
-  shelters = [];
+  shelters: Shelter[] = [
+    new Shelter(
+      "Greater Derry Humane Society",
+      "Some Street",
+      "Derry",
+      "NH",
+      "02144",
+      "555-555-5555",
+      "gdhs@gmail.com"
+    ),
+    new Shelter(
+      "Angel Memorial Shelter",
+      "Some Street",
+      "Boston",
+      "MA",
+      "02144",
+      "555-555-5555",
+      "angell@gmail.com"
+    ),
+    new Shelter(
+      "Mournful Paws",
+      "39 Sycamore Street",
+      "Somerville",
+      "MA",
+      "02143",
+      "555-555-5555",
+      "theodosia@gmail.com"
+    )
+  ];
 
   constructor() {}
 
