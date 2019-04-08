c2f4750a2e8123731784a3f6da54fc5a069531fc
Connie Hirsch <connie.hirsch@gmail.com>
2019-04-08 15:44:08 -0400

    Works by using a different name for Firebase?

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,18 +3,39 @@ import { HttpClient } from '@angular/common/http';
 import { Http } from '@angular/http';
 
 import { AdopteesService } from '../adoptees/adoptees.service';
+import { ShelterListService } from '../shelter-list/shelter-list.service';
 
 @Injectable()
 export class DataStorageService {
   constructor(private http: Http,
-    private adopteesService: AdopteesService) { }
+    private adopteesService: AdopteesService,
+    private shelterService: ShelterListService) { }
 
   storeAdoptees() {
     console.log(this.adopteesService.getAdoptees());
 
     return this.http.put(
-      'https://ng-pet-adoption.firebaseio.com/adoptions.json',
+      'https://shelters-a58f2.firebaseio.com/petAdoptions.json',
+      //      'https://ng-pet-adoption.firebaseio.com/adoptions.json',
       this.adopteesService.getAdoptees());
+  }
+
+  storeShelters() {
+    // console.log(this.shelterService.getShelters());
+    // return this.http.put(
+    //   'https://shelters-a58f2.firebaseio.com/shelters.json',
+    //   this.shelterService.getShelters()
+    // )
+
+    console.log(this.adopteesService.getAdoptees());
+
+    return this.http.put(
+      'https://shelters-a58f2.firebaseio.com/petAdoptions.json',
+      this.adopteesService.getAdoptees()
+    )
+
+    //https://shelters-a58f2.firebaseio.com/
+
   }
 
 }
