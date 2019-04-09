d4d4a6117cb49ece5a8fac0ef67512e59a09510c
Connie Hirsch <connie.hirsch@gmail.com>
2019-04-09 16:56:19 -0400

    Fetch and Store both working correctly

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -6,26 +6,32 @@ import { AdopteesService } from '../adoptees/adoptees.service';
 import { ShelterListService } from '../shelter-list/shelter-list.service';
 import { Adoptees } from '../adoptees/adoptees.model';
 import { Shelter } from './shelter.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
   constructor(private http: Http,
     private adopteesService: AdopteesService,
-    private shelterService: ShelterListService) { }
+    private shelterService: ShelterListService,
+    private authService: AuthService) { }
 
   storeAdoptees() {
+    const token = this.authService.getToken();
+
     console.log(this.adopteesService.getAdoptees());
 
     return this.http.put(
-      'https://shelters-a58f2.firebaseio.com/petAdoptions.json',
+      'https://shelters-a58f2.firebaseio.com/petAdoptions.json?auth=' + token,
       //      'https://ng-pet-adoption.firebaseio.com/adoptions.json',
       this.adopteesService.getAdoptees());
   }
 
   storeShelters() {
+    const token = this.authService.getToken();
+
     console.log(this.shelterService.getShelters());
     return this.http.put(
-      'https://shelters-a58f2.firebaseio.com/shelters.json',
+      'https://shelters-a58f2.firebaseio.com/shelters.json?auth=' + token,
       this.shelterService.getShelters()
     )
   }
@@ -34,8 +40,10 @@ export class DataStorageService {
 
 
   fetchPets() {
+    const token = this.authService.getToken();
+
     this.http.get(
-      'https://shelters-a58f2.firebaseio.com/petAdoptions.json')
+      'https://shelters-a58f2.firebaseio.com/petAdoptions.json?auth=' + token)
       .subscribe(
         (response: Response) => {
           const adoptees: Adoptees[] = response.json();
@@ -47,8 +55,10 @@ export class DataStorageService {
   }
 
   fetchShelters() {
+    const token = this.authService.getToken();
+
     this.http.get(
-      'https://shelters-a58f2.firebaseio.com/shelters.json')
+      'https://shelters-a58f2.firebaseio.com/shelters.json?auth=' + token)
       .subscribe(
         (response: Response) => {
           const shelters: Shelter[] = response.json();
