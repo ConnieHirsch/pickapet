f8bf45874b263a2d6a8020575bb4d25a7652da67
Connie Hirsch <connie.hirsch@gmail.com>
2019-05-08 18:12:06 -0400

    Several attempts to create working websites

--- src/app/adoptees/adoptees-detail/adoptees-detail.component.ts
+++ src/app/adoptees/adoptees-detail/adoptees-detail.component.ts
@@ -1,45 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
-import { Adoptees } from '../adoptees.model';
-import { AdopteesService } from '../adoptees.service';
-import { AuthService } from 'src/app/auth/auth.service';
+import { Adoptees } from "../adoptees.model";
+import { AdopteesService } from "../adoptees.service";
+import { AuthService } from "src/app/auth/auth.service";
 
 @Component({
-  selector: 'app-adoptees-detail',
-  templateUrl: './adoptees-detail.component.html',
-  styleUrls: ['./adoptees-detail.component.css']
+  selector: "app-adoptees-detail",
+  templateUrl: "./adoptees-detail.component.html",
+  styleUrls: ["./adoptees-detail.component.css"]
 })
 export class AdopteesDetailComponent implements OnInit {
   adoptee: Adoptees;
   id: number;
 
-  constructor(private adopteesService: AdopteesService,
-    private authService: AuthService,
+  constructor(
+    private adopteesService: AdopteesService,
+    public authService: AuthService,
     private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
   ngOnInit() {
     //const id = this.route.snapshot.params['id'];
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.id = +params['id'];
-          this.adoptee = this.adopteesService.getAdoptee(this.id);
-        });
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      this.adoptee = this.adopteesService.getAdoptee(this.id);
+    });
   }
 
   onEditAdoptee() {
-    this.router.navigate(['edit'], { relativeTo: this.route });
+    this.router.navigate(["edit"], { relativeTo: this.route });
     //this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route });
   }
 
   onDeleteAdoptee() {
     this.adopteesService.deleteAdoptee(this.id);
-    this.router.navigate(['/adoptees']);
+    this.router.navigate(["/adoptees"]);
   }
 
   onCloseDetail() {
-    this.router.navigate(['/']);
+    this.router.navigate(["/"]);
   }
 }
--- src/app/adoptees/adoptees-list/adoptees-list.component.ts
+++ src/app/adoptees/adoptees-list/adoptees-list.component.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
 
-import { Adoptees } from '../adoptees.model';
-import { AdopteesService } from '../adoptees.service';
-import { AuthService } from 'src/app/auth/auth.service';
+import { Adoptees } from "../adoptees.model";
+import { AdopteesService } from "../adoptees.service";
+import { AuthService } from "src/app/auth/auth.service";
 
 @Component({
-  selector: 'app-adoptees-list',
-  templateUrl: './adoptees-list.component.html',
-  styleUrls: ['./adoptees-list.component.css']
+  selector: "app-adoptees-list",
+  templateUrl: "./adoptees-list.component.html",
+  styleUrls: ["./adoptees-list.component.css"]
 })
 export class AdopteesListComponent implements OnInit, OnDestroy {
   adoptees: Adoptees[];
   subscription: Subscription;
 
-  constructor(private adopteesService: AdopteesService,
+  constructor(
+    private adopteesService: AdopteesService,
     private router: Router,
     private route: ActivatedRoute,
-    private authService: AuthService) { }
+    public authService: AuthService
+  ) {}
 
   ngOnInit() {
-    this.subscription = this.adopteesService.adopteesChanged
-      .subscribe(
-        (adoptees: Adoptees[]) => {
-          this.adoptees = adoptees;
-        }
-      );
+    this.subscription = this.adopteesService.adopteesChanged.subscribe(
+      (adoptees: Adoptees[]) => {
+        this.adoptees = adoptees;
+      }
+    );
     this.adoptees = this.adopteesService.getAdoptees();
   }
 
@@ -35,6 +36,6 @@ export class AdopteesListComponent implements OnInit, OnDestroy {
   }
 
   onNewAdoptee() {
-    this.router.navigate(['new'], { relativeTo: this.route });
+    this.router.navigate(["new"], { relativeTo: this.route });
   }
 }
