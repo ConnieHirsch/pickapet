1eeca8a033ad4c652a2ba324ce507d8ff0532f5f
Connie Hirsch <connie.hirsch@gmail.com>
2019-05-07 21:45:35 -0400

    At last header collapse working OK.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,32 +1,47 @@
-import { Component } from '@angular/core';
-import { Response } from '@angular/http';
+import { Component, HostListener } from "@angular/core";
+import { Response } from "@angular/http";
 
-import { DataStorageService } from '../shared/data-storage.service';
-import { AuthService } from '../auth/auth.service';
+import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent {
+  // navbar collapse for menu
+  public isCollapsed = false;
+  screenHeight: any;
+  screenWidth: any;
 
-  constructor(private dataStorageService: DataStorageService,
-    public authService: AuthService) { }
+  constructor(
+    private dataStorageService: DataStorageService,
+    public authService: AuthService
+  ) {
+    this.onResize();
+  }
 
+  @HostListener("window:resize", ["$event"])
+  onResize(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth == 800) {
+      console.log(this.screenHeight, this.screenWidth);
+    }
+    if (this.screenWidth > 800) {
+      this.isCollapsed = false;
+    } else {
+      this.isCollapsed = true;
+    }
+  }
   onSaveData() {
-    this.dataStorageService.storeAdoptees()
-      .subscribe(
-        (response: Response) => {
-          console.log(response);
-        }
-      );
-    this.dataStorageService.storeShelters()
-      .subscribe(
-        (response: Response) => {
-          console.log(response);
-        }
-      );
+    this.dataStorageService.storeAdoptees().subscribe((response: Response) => {
+      console.log(response);
+    });
+    this.dataStorageService.storeShelters().subscribe((response: Response) => {
+      console.log(response);
+    });
   }
 
   onFetchData() {
